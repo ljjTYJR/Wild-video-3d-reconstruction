483897e01fb769fe9b541ede950a8d3483337518
Shuo <shuo.sun@oru.se>
2024-09-28 17:29:40 +0200

    Add DPVO compiliation
    Signed-off-by: Shuo <shuo.sun@oru.se>

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 import os.path as osp
@@ -10,7 +10,7 @@ setup(
         CUDAExtension('droid_backends',
             include_dirs=[osp.join(ROOT, 'thirdparty/eigen')],
             sources=[
-                'src/droid.cpp', 
+                'src/droid.cpp',
                 'src/droid_kernels.cu',
                 'src/correlation_kernels.cu',
                 'src/altcorr_kernel.cu',
@@ -37,25 +37,55 @@ setup(
     packages=['lietorch'],
     package_dir={'': 'thirdparty/lietorch'},
     ext_modules=[
-        CUDAExtension('lietorch_backends', 
+        CUDAExtension('lietorch_backends',
             include_dirs=[
-                osp.join(ROOT, 'thirdparty/lietorch/lietorch/include'), 
+                osp.join(ROOT, 'thirdparty/lietorch/lietorch/include'),
                 osp.join(ROOT, 'thirdparty/eigen')],
             sources=[
-                'thirdparty/lietorch/lietorch/src/lietorch.cpp', 
+                'thirdparty/lietorch/lietorch/src/lietorch.cpp',
                 'thirdparty/lietorch/lietorch/src/lietorch_gpu.cu',
                 'thirdparty/lietorch/lietorch/src/lietorch_cpu.cpp'],
             extra_compile_args={
-                'cxx': ['-O2'], 
+                'cxx': ['-O2'],
                 'nvcc': ['-O2',
-                    '-gencode=arch=compute_60,code=sm_60', 
-                    '-gencode=arch=compute_61,code=sm_61', 
-                    '-gencode=arch=compute_70,code=sm_70', 
+                    '-gencode=arch=compute_60,code=sm_60',
+                    '-gencode=arch=compute_61,code=sm_61',
+                    '-gencode=arch=compute_70,code=sm_70',
                     '-gencode=arch=compute_75,code=sm_75',
                     '-gencode=arch=compute_80,code=sm_80',
-                    '-gencode=arch=compute_86,code=sm_86',                 
+                    '-gencode=arch=compute_86,code=sm_86',
                 ]
             }),
     ],
     cmdclass={ 'build_ext' : BuildExtension }
 )
+
+setup(
+    name='dpvo',
+    packages=find_packages(),
+    ext_modules=[
+        CUDAExtension('cuda_corr',
+            sources=['dpvo/altcorr/correlation.cpp', 'dpvo/altcorr/correlation_kernel.cu'],
+            extra_compile_args={
+                'cxx':  ['-O3'],
+                'nvcc': ['-O3'],
+            }),
+        CUDAExtension('cuda_ba',
+            sources=['dpvo/fastba/ba.cpp', 'dpvo/fastba/ba_cuda.cu'],
+            extra_compile_args={
+                'cxx':  ['-O3'],
+                'nvcc': ['-O3'],
+            }),
+        CUDAExtension('lietorch_backends',
+            include_dirs=[
+                osp.join(ROOT, 'dpvo/lietorch/include'),
+                osp.join(ROOT, 'thirdparty/eigen-3.4.0')],
+            sources=[
+                'dpvo/lietorch/src/lietorch.cpp',
+                'dpvo/lietorch/src/lietorch_gpu.cu',
+                'dpvo/lietorch/src/lietorch_cpu.cpp'],
+            extra_compile_args={'cxx': ['-O3'], 'nvcc': ['-O3'],}),
+    ],
+    cmdclass={
+        'build_ext': BuildExtension
+    })
